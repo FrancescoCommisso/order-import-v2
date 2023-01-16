a1331f613356a55a816e2b8f47bed1e20ca3709b
Developer <francescocommissodev@gmail.com>
2023-01-16 16:03:23 -0500

    finished front end funcitonality for order searchbar

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import "./App.css";
 import { withAuthenticator } from "@aws-amplify/ui-react";
-
+import { Amplify } from "aws-amplify";
 import "@aws-amplify/ui-react/styles.css";
 
 import awsExports from "./aws-exports";
 import ShopifyOrderInput from "./components/ShopifyOrderInput";
+import { useState } from "react";
 Amplify.configure(awsExports);
 
 const App = ({ signOut, user }) => {
+  const [shopifyOrder, setShopifyOrder] = useState(null);
+  const [rawOrder, setRawOrder] = useState(null);
+
   return (
     <div className="App">
       <p>Order Import Client V2</p>
 
       <p>{user.attributes.email}</p>
       <button onClick={signOut}>Sign out</button>
-      <ShopifyOrderInput></ShopifyOrderInput>
+      <ShopifyOrderInput
+        setRawOrder={setRawOrder}
+        setShopifyOrder={setShopifyOrder}
+      ></ShopifyOrderInput>
     </div>
   );
 };
--- src/components/ShopifyOrderInput/index.js
+++ src/components/ShopifyOrderInput/index.js
@@ -1,15 +1,66 @@
 import React, { useState } from "react";
 import { API, graphqlOperation } from "aws-amplify";
 
-const ShopifyOrderInput = () => {
+const ShopifyOrderInput = ({ setShopifyOrder, setRawOrder }) => {
   const [orderUrl, setOrderUrl] = useState("");
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
 
-  const fetchShopifyOrder = async () => {};
+  const fetchShopifyOrder = async () => {
+    setLoading(true);
+    setShopifyOrder(null);
+    setRawOrder(null);
+    setError("");
+
+    const orderId = parseOrderId({ orderUrl });
+
+    if (orderId) {
+      try {
+        console.log("running", orderId);
+        const { shopifyOrder, rawOrder } = await API.post(
+          "orderImportApi",
+          "/shopifyOrder",
+          {
+            body: { orderId },
+          }
+        );
+        console.log(shopifyOrder);
+        console.log(rawOrder);
+
+        setShopifyOrder(shopifyOrder);
+        setRawOrder(rawOrder);
+      } catch (e) {
+        console.log(e);
+        setError(e.message);
+      }
+    }
+
+    setLoading(false);
+  };
+
+  const parseOrderId = ({ orderUrl }) => {
+    try {
+      const orderIdMatch = orderUrl.match(/\d{13}/);
+      const orderId = orderIdMatch[0];
+      return orderId;
+    } catch (e) {
+      setError("Invalid Order URL");
+    }
+  };
 
   return (
     <div>
-      <input placeholder="order url"></input>
-      <button>Search</button>
+      <input
+        onChange={(e) => {
+          setError("");
+          setOrderUrl(e.target.value);
+        }}
+        placeholder="order url"
+      ></input>
+      <button disabled={loading} onClick={fetchShopifyOrder}>
+        Search
+      </button>
+      <p>{error}</p>
     </div>
   );
 };
